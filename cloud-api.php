10e6166dac229f3c5eba45a79e9c8391533aa3f9
Nattanon Kh <nattanondev@gmail.com>
2023-10-01 22:47:50 +0700

    cloudflare img v0.1

---
+++ cloud-api.php
@@ -0,0 +1,4 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" id="file">
+    <input type="submit" value="Upload" name="submit">
+</form>
--- upload.php
+++ upload.php
@@ -1,3 +1,26 @@
 <?php 
-    require_once __DIR__.'/vendor/autoload.php';
-    $cloudFlareImages = new Images('1adf66719c0e0ef72e53038acebcc018', 'x2skj57v2poPW8UxIQGqBACBxkJ4Glg42lVhbDPe', 'FG9yH3i4rybjZWgNeKKJvA');
+
+    $image = $_FILES["file"]["tmp_name"];
+    $url = 'https://api.cloudflare.com/client/v4/accounts/1adf66719c0e0ef72e53038acebcc018/images/v1';
+
+    echo $image;
+
+    $data = array(
+        'file' => $image
+    );
+
+    $headers = [ 
+        'Authorization: Bearer x2skj57v2poPW8UxIQGqBACBxkJ4Glg42lVhbDPe',
+        'Content-Type: application/json'
+    ];
+    
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    print_r($response);
